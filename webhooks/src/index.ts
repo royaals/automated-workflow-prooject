b04662fd21aeb122e481a37945b214dc79272778
Royal <royal.21ai040@sode-edu.in>
2024-08-31 21:26:29 +0530

    add prisma

--- webhooks/src/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { json } from 'body-parser';
-const app = express();
-app.use(json());
-app.post('/', (req, res) => {
-  console.log(req.body);
-  res.send('HOME PAGE');
-});
